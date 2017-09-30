64e559184c0d45b49b8bdbece3cac285b398ccfd
rchronic <radhityachronicle@gmail.com>
2017-09-30 19:13:15 +0700

    First commit

---
+++ apap_tutorial_4/src/main/java/com/example/tutorial4/ApapTutorial4Application.java
@@ -0,0 +1,12 @@
+package com.example.tutorial4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapTutorial4Application.class, args);
+	}
+}
